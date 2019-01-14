23fa60c3009e8e71df1e8fe006c61dbc7541dc61
Marcin Iskra <dunravel@gmail.com>
2019-01-14 23:45:25 +0100

    Test: Should calculateFileList return list of files to download

--- src/test/java/pl/parser/nbp/TestNbpConnector.java
+++ src/test/java/pl/parser/nbp/TestNbpConnector.java
@@ -1,4 +1,22 @@
 package pl.parser.nbp;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class TestNbpConnector {
+    @Test
+    public void shouldCalculateFileListReturnCorrectListOfFilenames(){
+        //given
+        String startDate = "2018-01-01";
+        String endDate = "2018-01-10";
+        NbpConnector nbpConnector = new NbpConnector();
+        //when
+        List<String> fileLists = nbpConnector.calculateFileList(startDate,endDate);
+        //then
+        Assert.assertEquals(fileLists, Arrays.asList("dir2018.txt"));
+
+    }
 }
