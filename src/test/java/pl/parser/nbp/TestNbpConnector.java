3a1e940040aac96f8258e408d9c531557ab998ed
Marcin Iskra <dunravel@gmail.com>
2019-01-14 23:50:38 +0100

    Refactor: Changed name for test

--- src/test/java/pl/parser/nbp/TestNbpConnector.java
+++ src/test/java/pl/parser/nbp/TestNbpConnector.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class TestNbpConnector {
     @Test
-    public void shouldCalculateFileListReturnCorrectListOfFilenames(){
+    public void shouldCalculateFileListReturnCorrectListOfFilenamesForPreviousYear(){
         //given
         String startDate = "2018-01-01";
         String endDate = "2018-01-10";
@@ -17,6 +17,6 @@ public class TestNbpConnector {
         List<String> fileLists = nbpConnector.calculateFileList(startDate,endDate);
         //then
         Assert.assertEquals(fileLists, Arrays.asList("dir2018.txt"));
-
     }
+
 }
