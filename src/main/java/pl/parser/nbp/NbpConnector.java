f396a8f151e4535403f76d7edd7b9f0571be7c10
Marcin Iskra <dunravel@gmail.com>
2019-01-14 23:48:46 +0100

    Implement: calculateFileList returns list of files to download

--- src/main/java/pl/parser/nbp/NbpConnector.java
+++ src/main/java/pl/parser/nbp/NbpConnector.java
@@ -1,4 +1,19 @@
 package pl.parser.nbp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class NbpConnector {
+    public List<String> calculateFileList(String startDate, String endDate) {
+        int startYear = Integer.parseInt(startDate.substring(0,4));
+        int endYear = Integer.parseInt(endDate.substring(0,4));
+
+        List<String> fileList = new ArrayList<>();
+
+        for(int i = startYear; i<=endYear; i+=1){
+            fileList.add("dir"+i+".txt");
+        }
+
+        return fileList;
+    }
 }
