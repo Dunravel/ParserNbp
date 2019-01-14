7ac4a4d3d147ea8eb3f7f5e8993b2c3f82c05023
Marcin Iskra <dunravel@gmail.com>
2019-01-15 00:07:45 +0100

    Refactor: Formatted lines

--- src/test/java/pl/parser/nbp/TestNbpConnector.java
+++ src/test/java/pl/parser/nbp/TestNbpConnector.java
@@ -15,33 +15,33 @@ public class TestNbpConnector {
     private int previousYear;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         nbpConnector = new NbpConnector();
         currentYear = LocalDate.now().getYear();
         previousYear = currentYear - 1;
     }
 
     @Test
-    public void shouldCalculateFileListReturnCorrectListOfFilenamesForPreviousYear(){
+    public void shouldCalculateFileListReturnCorrectListOfFilenamesForPreviousYear() {
         //given
-        String startDate = previousYear+"-01-01";
-        String endDate = previousYear+"-01-10";
+        String startDate = previousYear + "-01-01";
+        String endDate = previousYear + "-01-10";
 
         //when
-        List<String> fileLists = nbpConnector.calculateFileList(startDate,endDate);
+        List<String> fileLists = nbpConnector.calculateFileList(startDate, endDate);
         //then
-        Assert.assertEquals(fileLists, Arrays.asList("dir"+previousYear+".txt"));
+        Assert.assertEquals(fileLists, Arrays.asList("dir" + previousYear + ".txt"));
     }
 
     @Test
-    public void shouldCalculateFileListReturnCorrectListOfFilenamesForCurrentYear(){
+    public void shouldCalculateFileListReturnCorrectListOfFilenamesForCurrentYear() {
         //given
         String startDate = previousYear + "-01-01";
         String endDate = currentYear + "-01-01";
         //when
-        List<String> fileLists = nbpConnector.calculateFileList(startDate,endDate);
+        List<String> fileLists = nbpConnector.calculateFileList(startDate, endDate);
         //then
-        Assert.assertEquals(fileLists,Arrays.asList("dir"+ previousYear +".txt","dir.txt"));
+        Assert.assertEquals(fileLists, Arrays.asList("dir" + previousYear + ".txt", "dir.txt"));
     }
 
 }
