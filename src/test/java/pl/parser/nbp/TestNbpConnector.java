a1affbf4b14413a9099e23212846edcce1d04232
Marcin Iskra <dunravel@gmail.com>
2019-01-15 00:06:32 +0100

    Refactor: Extracted calculation of previous and current year to setUp

--- src/test/java/pl/parser/nbp/TestNbpConnector.java
+++ src/test/java/pl/parser/nbp/TestNbpConnector.java
@@ -11,36 +11,37 @@ import java.util.List;
 public class TestNbpConnector {
 
     private NbpConnector nbpConnector;
+    private int currentYear;
+    private int previousYear;
 
     @Before
     public void setUp(){
         nbpConnector = new NbpConnector();
+        currentYear = LocalDate.now().getYear();
+        previousYear = currentYear - 1;
     }
 
     @Test
     public void shouldCalculateFileListReturnCorrectListOfFilenamesForPreviousYear(){
         //given
-        String startDate = "2018-01-01";
-        String endDate = "2018-01-10";
+        String startDate = previousYear+"-01-01";
+        String endDate = previousYear+"-01-10";
 
         //when
         List<String> fileLists = nbpConnector.calculateFileList(startDate,endDate);
         //then
-        Assert.assertEquals(fileLists, Arrays.asList("dir2018.txt"));
+        Assert.assertEquals(fileLists, Arrays.asList("dir"+previousYear+".txt"));
     }
 
     @Test
     public void shouldCalculateFileListReturnCorrectListOfFilenamesForCurrentYear(){
         //given
-
-        int currentYear = LocalDate.now().getYear();
-        int previousYear = currentYear - 1;
         String startDate = previousYear + "-01-01";
         String endDate = currentYear + "-01-01";
         //when
         List<String> fileLists = nbpConnector.calculateFileList(startDate,endDate);
         //then
-        Assert.assertEquals(fileLists,Arrays.asList("dir"+previousYear+".txt","dir.txt"));
+        Assert.assertEquals(fileLists,Arrays.asList("dir"+ previousYear +".txt","dir.txt"));
     }
 
 }
