718c1febbb9d565e051416c2d449546ec6702b20
Marcin Iskra <dunravel@gmail.com>
2019-01-14 19:56:07 +0100

    Implement: Return IncorrectAmountOfParameters error if number of parameters other than three

--- src/main/java/pl/parser/nbp/InputCheck.java
+++ src/main/java/pl/parser/nbp/InputCheck.java
@@ -4,6 +4,11 @@ class InputCheck {
 
 
     InputCheck(String[] args) {
+        verifyAmount(args);
+    }
 
+    private void verifyAmount(String[] args) {
+        if(args.length != 3)
+            throw new IncorrectAmountOfParameters();
     }
 }
