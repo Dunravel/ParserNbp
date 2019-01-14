0260b00108b3fdc6490a83904bd91dc2fd82d983
Marcin Iskra <dunravel@gmail.com>
2019-01-14 23:56:46 +0100

    Implement: calculateFileList returns correct file list for previous and current year

--- src/main/java/pl/parser/nbp/NbpConnector.java
+++ src/main/java/pl/parser/nbp/NbpConnector.java
@@ -1,17 +1,22 @@
 package pl.parser.nbp;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 class NbpConnector {
-    public List<String> calculateFileList(String startDate, String endDate) {
+    List<String> calculateFileList(String startDate, String endDate) {
         int startYear = Integer.parseInt(startDate.substring(0,4));
         int endYear = Integer.parseInt(endDate.substring(0,4));
 
         List<String> fileList = new ArrayList<>();
 
         for(int i = startYear; i<=endYear; i+=1){
-            fileList.add("dir"+i+".txt");
+            if(i != LocalDate.now().getYear()) {
+                fileList.add("dir" + i + ".txt");
+            }else{
+                fileList.add("dir.txt");
+            }
         }
 
         return fileList;
