6efeb336282958108715883269a6ff3d9f5d0040
Marcin Iskra <dunravel@gmail.com>
2019-01-16 22:47:40 +0100

    Refactor: Moved file name validation to separate class: FileNameCheck; Extracted some variables to constants; Deleted println

--- src/main/java/pl/parser/nbp/NbpConnector.java
+++ src/main/java/pl/parser/nbp/NbpConnector.java
@@ -7,6 +7,9 @@ import java.util.List;
 class NbpConnector {
 
     private static final String REQUIRED_TABLE_TYPE = "c";
+    private static final String CATALOG_NAME_HEADER = "dir";
+    private static final String CATALOG_NAME_FOOTER = ".txt";
+    private static final String CURRENT_YEAR_CATALOG_NAME = "dir.txt";
 
     List<String> createCatalogList(String startDate, String endDate) {
         int startYear = Integer.parseInt(startDate.substring(0,4));
@@ -14,11 +17,11 @@ class NbpConnector {
 
         List<String> fileList = new ArrayList<>();
 
-        for(int i = startYear; i<=endYear; i+=1){
-            if(i != LocalDate.now().getYear()) {
-                fileList.add("dir" + i + ".txt");
+        for(int year = startYear; year<=endYear; year+=1){
+            if(year != LocalDate.now().getYear()) {
+                fileList.add(CATALOG_NAME_HEADER + year + CATALOG_NAME_FOOTER);
             }else{
-                fileList.add("dir.txt");
+                fileList.add(CURRENT_YEAR_CATALOG_NAME);
             }
         }
         return fileList;
