299bcbbdaabe890085d8def849ec9cf70c7d87db
Marcin Iskra <dunravel@gmail.com>
2019-01-17 08:58:54 +0100

    Test: Should getCorrectFileName return null if file name wrong wrong table

--- src/test/java/pl/parser/nbp/TestFileNameFilter.java
+++ src/test/java/pl/parser/nbp/TestFileNameFilter.java
@@ -9,9 +9,22 @@ public class TestFileNameFilter {
         //given
         String startDate = "2018-12-31";
         FileNameFilter fileNameFilter = new FileNameFilter(startDate,startDate);
+        String correctFileName = "c252z181231.xml";
         //when
-        String name = fileNameFilter.getCorrectFileName("c252z181231.xml");
+        String name = fileNameFilter.getCorrectFileName(correctFileName);
         //then
         Assert.assertEquals("c252z181231.xml",name);
+    }
+
+    @Test
+    public void shouldGetCorrectFileNameReturnNullWhenFileWithNotCorrectTable(){
+        //given
+        String startDate = "2018-12-31";
+        FileNameFilter fileNameFilter = new FileNameFilter(startDate,startDate);
+        String wrongTableFileName = "h252z181231.xml";
+        //when
+        String name = fileNameFilter.getCorrectFileName(wrongTableFileName);
+        //then
+        Assert.assertNull(name);
     }
 }
