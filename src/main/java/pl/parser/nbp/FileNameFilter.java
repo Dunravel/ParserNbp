6f275477c2a299633f96f6e976b9e664374e9ebb
Marcin Iskra <dunravel@gmail.com>
2019-01-17 09:00:13 +0100

    Implement: getCorrectFileName returns null if file with wrong table

--- src/main/java/pl/parser/nbp/FileNameFilter.java
+++ src/main/java/pl/parser/nbp/FileNameFilter.java
@@ -1,21 +1,36 @@
 package pl.parser.nbp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class FileNameFilter {
-    private String tableType;
+    private static final String REQUIRED_TABLE_TYPE = "c";
     private String startDate;
     private String endDate;
 
-    FileNameFilter(String tableType, String startDate, String endDate){
-        this.tableType = tableType;
+    FileNameFilter(String startDate, String endDate){
         this.startDate = startDate.substring(2,4) + startDate.substring(5,7) + startDate.substring(8,10);
         this.endDate = endDate.substring(2,4) + endDate.substring(5,7) + endDate.substring(8,10);
     }
+
+    public List<String> filter(List<String> fileList) {
+        List<String> filteredFileList = new ArrayList<>();
+        for (String fileNameRaw : fileList) {
+            String name = getCorrectFileName(fileNameRaw);
+            if(name != null) {
+                filteredFileList.add(name);
+            }
+        }
+
+        return filteredFileList;
+    }
+
     String getCorrectFileName(String input) {
         input = input.replace("\uFEFF", "");
-        if (input.startsWith("c")) {
-            if (input.substring(5, 11).compareTo(startDate) >= 0 && input.substring(5, 11).compareTo(endDate) <= 0) {
+        if (input.startsWith(REQUIRED_TABLE_TYPE)) {
+
                 return input;
-            }
+
         }
         return null;
     }
