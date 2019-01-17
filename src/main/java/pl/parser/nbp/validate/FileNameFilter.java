54f9c1872903b6593289ac51f75c4d1d3a4dae0b
Marcin Iskra <dunravel@gmail.com>
2019-01-17 16:19:33 +0100

    Refactor: Moved table type from constant to variable

--- src/main/java/pl/parser/nbp/validate/FileNameFilter.java
+++ src/main/java/pl/parser/nbp/validate/FileNameFilter.java
@@ -1,16 +1,17 @@
 package pl.parser.nbp.validate;
 
-import pl.parser.nbp.NoFilesFoundException;
+import pl.parser.nbp.NoFileFoundException;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class FileNameFilter {
-    private static final String REQUIRED_TABLE_TYPE = "c";
+    private String tableType;
     private String startDate;
     private String endDate;
 
-    public FileNameFilter(String startDate, String endDate){
+    public FileNameFilter(String tableType, String startDate, String endDate){
+        this.tableType = tableType;
         this.startDate = startDate.substring(2,4) + startDate.substring(5,7) + startDate.substring(8,10);
         this.endDate = endDate.substring(2,4) + endDate.substring(5,7) + endDate.substring(8,10);
     }
@@ -24,14 +25,14 @@ public class FileNameFilter {
             }
         }
         if(filteredFileList.size()  == 0){
-            throw new NoFilesFoundException();
+            throw new NoFileFoundException();
         }
         return filteredFileList;
     }
 
     public String getCorrectFileName(String input) {
         input = input.replace("\uFEFF", "");
-        if (input.startsWith(REQUIRED_TABLE_TYPE)) {
+        if (input.startsWith(tableType)) {
             if (input.substring(5, 11).compareTo(startDate) >= 0 && input.substring(5, 11).compareTo(endDate) <= 0) {
                 return input;
             }
