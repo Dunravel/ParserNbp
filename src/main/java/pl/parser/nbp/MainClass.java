0406ddb08db4256ff1b2c321ea911f514596f7f2
Marcin Iskra <dunravel@gmail.com>
2019-01-17 16:08:37 +0100

    Added creation of CurrencyData set to store data

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -1,6 +1,8 @@
 package pl.parser.nbp;
 
-import pl.parser.nbp.domain.CatalogList;
+import pl.parser.nbp.domain.Currency;
+import pl.parser.nbp.domain.CurrencyData;
+import pl.parser.nbp.domain.CurrencyDataFactory;
 import pl.parser.nbp.validate.InputCheck;
 
 import java.util.Set;
@@ -8,18 +10,20 @@ import java.util.Set;
 public class MainClass {
     public static void main(String[] args) {
 
-        //
-        args = new String[]{"EUR", "2018-12-31", "2019-01-03"};
         InputCheck inputCheck = new InputCheck();
         inputCheck.verify(args);
 
         String startDate = args[1];
         String endDate = args[2];
+        Currency currency = Currency.valueOf(args[0]);
 
         NbpData nbpData = new NbpData(startDate,endDate);
-        Set<String> catalogList = nbpData.createCatalogList();
-        Set<String> fileList = nbpData.createFileList(catalogList);
+        Set<String> fileList = nbpData.createFileList();
         System.out.println(fileList);
+
+        CurrencyDataFactory currencyDataFactory = new CurrencyDataFactory();
+        Set<CurrencyData> currencyDataSet = currencyDataFactory.create(currency,fileList);
+
 
     }
 }
