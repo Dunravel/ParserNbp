dc9546b3b794c3de5e28e791af27a991676a7f91
Marcin Iskra <dunravel@gmail.com>
2019-01-17 16:04:51 +0100

    Added CurrencyData class

---
+++ src/main/java/pl/parser/nbp/domain/CurrencyData.java
@@ -0,0 +1,48 @@
+package pl.parser.nbp.domain;
+
+class CurrencyData {
+    private final String fileName;
+    private final Currency currency;
+    private double buyRate;
+    private double sellRate;
+
+
+    CurrencyData(Currency currency, String sourceName) {
+        this.fileName = sourceName;
+        this.currency = currency;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public double getBuyRate() {
+        return buyRate;
+    }
+
+    public void setBuyRate(double buyRate) {
+        this.buyRate = buyRate;
+    }
+
+    public double getSellRate() {
+        return sellRate;
+    }
+
+    public void setSellRate(double sellRate) {
+        this.sellRate = sellRate;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyData{" +
+                "fileName='" + fileName + '\'' +
+                ", currency=" + currency +
+                ", buyRate=" + buyRate +
+                ", sellRate=" + sellRate +
+                '}';
+    }
+}
