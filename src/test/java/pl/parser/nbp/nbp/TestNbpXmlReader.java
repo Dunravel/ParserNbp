58e1d8d815866e193cf7c72ce6adc57338c10831
Marcin Iskra <dunravel@gmail.com>
2019-01-18 20:31:21 +0100

    Test: Should IsCurrency return true when correct sting matched

---
+++ src/test/java/pl/parser/nbp/nbp/TestNbpXmlReader.java
@@ -0,0 +1,35 @@
+package pl.parser.nbp.nbp;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+public class TestNbpXmlReader {
+
+    @Test
+    public void shouldIsCurrencyReturnTrueWhenRequestedCurrency(){
+        //given
+        String requestedCurrency = "EUR";
+        NbpXmlReader nbpXmlReader = new NbpXmlReader();
+        XMLEvent xmlEvent = Mockito.mock(XMLEvent.class);
+        StartElement startElement = Mockito.mock(StartElement.class);
+        QName qName = Mockito.mock(QName.class);
+        String matcher = "kod_waluty";
+        BDDMockito.given(xmlEvent.isStartElement()).willReturn(true);
+        BDDMockito.given(xmlEvent.asStartElement()).willReturn(startElement);
+        BDDMockito.given(startElement.getName()).willReturn(qName);
+        BDDMockito.given(qName.getLocalPart()).willReturn(matcher);
+        QName name = startElement.getName();
+        startElement.getName().getLocalPart();
+        //when
+        boolean isCurrecy = nbpXmlReader.isCurrency(requestedCurrency, xmlEvent);
+        //then
+        Assert.assertTrue(isCurrecy);
+
+    }
+}
