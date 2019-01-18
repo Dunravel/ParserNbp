26f84994dc459da1e5da96f43109b4d58a1a2724
Marcin Iskra <dunravel@gmail.com>
2019-01-18 18:33:42 +0100

    Test: Should getFileListFromCatalog return correct list;

--- src/test/java/pl/parser/nbp/nbp/TestNbpDownloader.java
+++ src/test/java/pl/parser/nbp/nbp/TestNbpDownloader.java
@@ -11,6 +11,8 @@ import org.mockito.internal.util.collections.Sets;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
 
 public class TestNbpDownloader {
@@ -22,13 +24,27 @@ public class TestNbpDownloader {
     }
 
     @Test
-    public void shouldGetFileListReturnCompleteListForCatalog() throws IOException {
+    public void shouldGetFileListReturnCompleteListForCatalog(){
         //given
         NbpCatalogConnector nbpCatalogConnector = Mockito.mock(NbpCatalogConnector.class);
         BufferedReader reader = new BufferedReader(new StringReader("first\nsecond\nthird"));
         BDDMockito.given(nbpCatalogConnector.getConnection(ArgumentMatchers.any())).willReturn(reader);
         //when
         Set<String> result = nbpDownloader.getFileList(nbpCatalogConnector,Sets.newSet("1"));
+        //then
+        Assert.assertEquals(3, result.size());
+        Assert.assertEquals(Sets.newSet("first.xml","second.xml","third.xml"),result);
+    }
+
+    @Test
+    public void shouldGetFileListFromCatalogReturnContentsOfSingleCatalog() throws MalformedURLException {
+        //given
+        NbpCatalogConnector nbpCatalogConnector = Mockito.mock(NbpCatalogConnector.class);
+        BufferedReader reader = new BufferedReader(new StringReader("first\nsecond\nthird"));
+        URL url = new URL("http://localhost/");
+        BDDMockito.given(nbpCatalogConnector.getConnection(ArgumentMatchers.any())).willReturn(reader);
+        //when
+        Set<String> result = nbpDownloader.getFileListFromCatalog(nbpCatalogConnector,url);
         //then
         Assert.assertEquals(3, result.size());
         Assert.assertEquals(Sets.newSet("first.xml","second.xml","third.xml"),result);
