76253fd5a50843f526c8e5ccc221c87acc33302a
Marcin Iskra <dunravel@gmail.com>
2019-01-18 11:31:49 +0100

    Implement: IncorrectDatePeriodException returns correct message

--- src/main/java/pl/parser/nbp/validate/IncorrectDatePeriodException.java
+++ src/main/java/pl/parser/nbp/validate/IncorrectDatePeriodException.java
@@ -1,4 +1,8 @@
 package pl.parser.nbp.validate;
 
 class IncorrectDatePeriodException extends RuntimeException{
+
+    IncorrectDatePeriodException(){
+        super("Date period not correct. Make sure that second date is same or later than the first one.");
+    }
 }
