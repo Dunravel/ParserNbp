52768a6ac9cf37defc3c28ecaba714c64d07a44d
Marcin Iskra <dunravel@gmail.com>
2019-01-18 10:53:22 +0100

    Implement: IncorrectAmountOfParametersException returns correct text

--- src/main/java/pl/parser/nbp/validate/IncorrectAmountOfParametersException.java
+++ src/main/java/pl/parser/nbp/validate/IncorrectAmountOfParametersException.java
@@ -2,4 +2,7 @@ package pl.parser.nbp.validate;
 
 class IncorrectAmountOfParametersException extends RuntimeException {
 
+    IncorrectAmountOfParametersException(){
+        super("Incorrect amount of parameters. Enter: [Currency] [Start date] [End date]\nExample: EUR 2013-01-01 2013-01-31");
+    }
 }
