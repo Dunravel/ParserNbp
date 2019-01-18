604cad480610454607915e9d034ff4201f5e7c71
Marcin Iskra <dunravel@gmail.com>
2019-01-18 16:48:49 +0100

    Test: Should getDeviationSellRate return correct deviation

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -1,19 +1,10 @@
 package pl.parser.nbp;
 
-import pl.parser.nbp.domain.*;
-import pl.parser.nbp.nbp.NbpData;
-import pl.parser.nbp.validate.FileNameFilter;
-import pl.parser.nbp.validate.InputCheck;
-
-import java.util.Set;
-
 public class MainClass {
-
-    public static final String CURRENCY_TABLE_TYPE = "c";
 
     public static void main(String[] args) {
 
-        args = new String[]{"EUR", "2013-01-28", "2013-01-31"};
+        //args = new String[]{"EUR", "2013-01-28", "2013-01-31"};
 
         Handler handler = new Handler();
         handler.run(args);
--- src/test/java/pl/parser/nbp/domain/TestCurrencyDataSet.java
+++ src/test/java/pl/parser/nbp/domain/TestCurrencyDataSet.java
@@ -9,7 +9,7 @@ public class TestCurrencyDataSet {
     @Test
     public void shouldGetAverageBuyRateReturnCorrectAverage(){
         //given
-        CurrencyDataSet currencyDataSet = new CurrencyDataSet(Sets.newSet(new CurrencyData(Currency.EUR,"first.txt"),new CurrencyData(Currency.EUR,"first.txt")));
+        CurrencyDataSet currencyDataSet = new CurrencyDataSet(Sets.newSet(new CurrencyData(Currency.EUR,"first.txt"),new CurrencyData(Currency.EUR,"second.txt")));
         double counter = 4;
         for(CurrencyData currencyData : currencyDataSet.getSet()){
             currencyData.setBuyRate(counter);
@@ -18,6 +18,24 @@ public class TestCurrencyDataSet {
         //when
         double averageBuyRate = currencyDataSet.getAverageBuyRate();
         //then
-        Assert.assertEquals(5,(int)averageBuyRate);
+        Assert.assertEquals(5.0,averageBuyRate,0.1);
+    }
+
+    @Test
+    public void shouldGetDeviationSellRateReturnCorrectDeviation(){
+        //given
+        CurrencyDataSet currencyDataSet = new CurrencyDataSet(Sets.newSet(new CurrencyData(Currency.EUR,"first.txt"),new CurrencyData(Currency.EUR,"second.txt"),new CurrencyData(Currency.EUR,"third.txt")));
+        double[] list = {10,20,30};
+        int counter = 0;
+        for(CurrencyData currencyData : currencyDataSet.getSet()){
+            currencyData.setSellRate(list[counter]);
+            counter++;
+        }
+        //when
+        double deviation = currencyDataSet.getDeviationSellRate();
+        System.out.println(deviation);
+        //then
+        Assert.assertEquals(8.1649,deviation,0.0001);
+
     }
 }
