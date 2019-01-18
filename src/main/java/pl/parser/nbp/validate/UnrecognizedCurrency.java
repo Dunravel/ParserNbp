6f52766cd8dfa60b3b8ad5a84d9a95b98e966456
Marcin Iskra <dunravel@gmail.com>
2019-01-18 11:42:50 +0100

    Implement: UnrecognizedCurrencyException returns correct message

--- src/main/java/pl/parser/nbp/validate/UnrecognizedCurrency.java
+++
@@ -1,5 +0,0 @@
-package pl.parser.nbp.validate;
-
-public class UnrecognizedCurrency extends RuntimeException {
-
-}
---
+++ src/main/java/pl/parser/nbp/validate/UnrecognizedCurrencyException.java
@@ -0,0 +1,9 @@
+package pl.parser.nbp.validate;
+
+import pl.parser.nbp.domain.Currency;
+
+class UnrecognizedCurrencyException extends RuntimeException {
+    UnrecognizedCurrencyException(){
+        super("Incorrect currency. \nSupported currencies: " + Currency.listAll());
+    }
+}
