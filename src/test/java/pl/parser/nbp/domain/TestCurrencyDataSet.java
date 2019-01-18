ace35f3710095c0a358ffbf1f575fd8373ea1383
Marcin Iskra <dunravel@gmail.com>
2019-01-18 16:52:41 +0100

    Refactor: Moved shared code to setUp method

--- src/test/java/pl/parser/nbp/domain/TestCurrencyDataSet.java
+++ src/test/java/pl/parser/nbp/domain/TestCurrencyDataSet.java
@@ -1,41 +1,49 @@
 package pl.parser.nbp.domain;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.internal.util.collections.Sets;
 
 
 public class TestCurrencyDataSet {
+
+    private CurrencyDataSet currencyDataSet;
+
+    @Before
+    public void setUp(){
+        currencyDataSet = new CurrencyDataSet(
+                Sets.newSet(
+                        new CurrencyData(Currency.EUR,"first.txt"),
+                        new CurrencyData(Currency.EUR,"second.txt"),
+                        new CurrencyData(Currency.EUR,"third.txt")
+                )
+        );
+
+        double[] list = {10,20,30};
+        int counter = 0;
+        for(CurrencyData currencyData : currencyDataSet.getSet()){
+            currencyData.setBuyRate(list[counter]);
+            currencyData.setSellRate(list[counter]);
+            counter++;
+        }
+    }
+
     @Test
     public void shouldGetAverageBuyRateReturnCorrectAverage(){
         //given
-        CurrencyDataSet currencyDataSet = new CurrencyDataSet(Sets.newSet(new CurrencyData(Currency.EUR,"first.txt"),new CurrencyData(Currency.EUR,"second.txt")));
-        double counter = 4;
-        for(CurrencyData currencyData : currencyDataSet.getSet()){
-            currencyData.setBuyRate(counter);
-            counter+=2;
-        }
         //when
         double averageBuyRate = currencyDataSet.getAverageBuyRate();
         //then
-        Assert.assertEquals(5.0,averageBuyRate,0.1);
+        Assert.assertEquals(20.0,averageBuyRate,0.1);
     }
 
     @Test
     public void shouldGetDeviationSellRateReturnCorrectDeviation(){
         //given
-        CurrencyDataSet currencyDataSet = new CurrencyDataSet(Sets.newSet(new CurrencyData(Currency.EUR,"first.txt"),new CurrencyData(Currency.EUR,"second.txt"),new CurrencyData(Currency.EUR,"third.txt")));
-        double[] list = {10,20,30};
-        int counter = 0;
-        for(CurrencyData currencyData : currencyDataSet.getSet()){
-            currencyData.setSellRate(list[counter]);
-            counter++;
-        }
         //when
         double deviation = currencyDataSet.getDeviationSellRate();
-        System.out.println(deviation);
         //then
         Assert.assertEquals(8.1649,deviation,0.0001);
-
     }
 }
