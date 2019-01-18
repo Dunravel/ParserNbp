6a790c7421f75ccb224d509668728759a04fd277
Marcin Iskra <dunravel@gmail.com>
2019-01-18 16:01:39 +0100

    Test: Should create() create CurrencyDataSet with set

---
+++ src/test/java/pl/parser/nbp/domain/TestCurrencyDataSetFactory.java
@@ -0,0 +1,21 @@
+package pl.parser.nbp.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.internal.util.collections.Sets;
+
+import java.util.Set;
+
+public class TestCurrencyDataSetFactory {
+
+    @Test
+    public void shouldCreateMethodCreateListOfCurrencyData(){
+        //given
+        CurrencyDataSetFactory currencyDataSetFactory = new CurrencyDataSetFactory();
+        Set<String> fileList = Sets.newSet("first.txt");
+        //when
+        CurrencyDataSet currencyDataSet = currencyDataSetFactory.create(Currency.EUR,fileList);
+        //then
+        Assert.assertEquals(1,currencyDataSet.getSet().size());
+    }
+}
