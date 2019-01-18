030d781b6c9302796cd4ff2a414be2ca5901951f
Marcin Iskra <dunravel@gmail.com>
2019-01-18 22:25:51 +0100

    Refactor: Removed not needed text

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -3,9 +3,6 @@ package pl.parser.nbp;
 public class MainClass {
 
     public static void main(String[] args) {
-
-        //args = new String[]{"EUR", "2013-01-28", "2013-01-31"};
-
         Handler handler = new Handler();
         handler.run(args);
     }
