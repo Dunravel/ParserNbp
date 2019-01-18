40da25c6bbb95e4ad86e741a13d5bb1b363afb70
Marcin Iskra <dunravel@gmail.com>
2019-01-18 16:15:15 +0100

    Refactor: Extracted common code to setUp

--- src/test/java/pl/parser/nbp/domain/TestCurrencyDataSetFactory.java
+++ src/test/java/pl/parser/nbp/domain/TestCurrencyDataSetFactory.java
@@ -1,6 +1,7 @@
 package pl.parser.nbp.domain;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.internal.util.collections.Sets;
 
@@ -8,24 +9,30 @@ import java.util.Set;
 
 public class TestCurrencyDataSetFactory {
 
+    private CurrencyDataSetFactory currencyDataSetFactory;
+
+    @Before
+    public void setUp(){
+        currencyDataSetFactory = new CurrencyDataSetFactory();
+    }
     @Test
     public void shouldCreateMethodCreateListOfCurrencyData() {
         //given
-        CurrencyDataSetFactory currencyDataSetFactory = new CurrencyDataSetFactory();
         Set<String> fileList = Sets.newSet("first.txt");
         //when
         CurrencyDataSet currencyDataSet = currencyDataSetFactory.create(Currency.EUR,fileList);
         //then
         Assert.assertEquals(1,currencyDataSet.getSet().size());
+        Assert.assertEquals("first.txt",currencyDataSet.getSet().iterator().next().getFileName());
     }
 
     @Test(expected = FileListEmptyException.class)
     public void shouldCreateReturnErrorWhenFileListEmpty() {
         //given
-        CurrencyDataSetFactory currencyDataSetFactory = new CurrencyDataSetFactory();
         Set<String> fileList = Sets.newSet();
         //when
         currencyDataSetFactory.create(Currency.EUR,fileList);
         //then
     }
+
 }
